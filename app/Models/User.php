0af05014f6cec598a93fd160fa77dd74af34589a
zacktagnan <zacktagnan@gmail.com>
2024-11-16 02:31:47 +0100

    Lvl.Pint-Formateo.archivos.PHP

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
     use HasProfilePhoto;
     use HasTeams;
     use Notifiable;
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,10 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
