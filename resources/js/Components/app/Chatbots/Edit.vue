cf6f4a811c3f5555dad1aa169a5f73e37696b4ba
zacktagnan <zacktagnan@gmail.com>
2024-11-29 01:03:57 +0100

    Chatbot-Edit-Formulario.preparado

--- resources/js/Components/app/Chatbots/Edit.vue
+++ resources/js/Components/app/Chatbots/Edit.vue
@@ -1,20 +1,33 @@
 <script setup>
 import PrimaryButton from '@/Components/PrimaryButton.vue'
 import { ArrowUturnLeftIcon } from '@heroicons/vue/24/outline'
-import { Link } from '@inertiajs/vue3'
+import { Link, useForm } from '@inertiajs/vue3'
 import FormSection from '@/Components/FormSection.vue'
+import Form from '@/Components/app/Chatbots/Form.vue'
 import ActionMessage from '@/Components/ActionMessage.vue'
 
-defineProps({
+const props = defineProps({
     chatbot: {
         type: Object,
         required: true,
     },
 })
+
+const form = useForm({
+    name: props.chatbot.name,
+    system_prompt: props.chatbot.system_prompt,
+    model: props.chatbot.model,
+    temperature: String(props.chatbot.temperature),
+})
+
+const handleSubmit = () => {
+    // console.log('FORM_DATA', form.data())
+    form.put(route('chatbots.update', props.chatbot.id))
+}
 </script>
 
 <template>
-    <FormSection>
+    <FormSection @submitted="handleSubmit">
         <template #title>{{ $t('app.chatbots.actions.edit.header.title') }}</template>
         <template #description>{{ $t('app.chatbots.actions.edit.header.description') }}</template>
         <template v-slot:backTo>
@@ -27,7 +40,9 @@ defineProps({
                 <span>{{ $t('app.chatbots.actions.back_to_list') }}</span>
             </Link>
         </template>
-        <template #form>FORM</template>
+        <template #form>
+            <Form :form="form" />
+        </template>
         <template #actions>
             <ActionMessage :on="false" class="me-3">{{ $t('app.chatbots.actions.edit.actions.updated') }}</ActionMessage>
             <PrimaryButton
---
+++ resources/js/Components/app/Chatbots/Form.vue
@@ -0,0 +1,41 @@
+<script setup>
+import InputLabel from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+
+defineProps({
+    form: {
+        type: Object,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <div class="col-span-6 sm:col-span-4">
+        <InputLabel for="name" value="Nombre" />
+        <TextInput id="name" v-model="form.name"
+            class="mt-1 block w-full"
+        />
+    </div>
+
+    <div class="col-span-6 sm:col-span-4">
+        <InputLabel for="system_prompt" value="System Prompt" />
+        <TextInput id="system_prompt" v-model="form.system_prompt"
+            class="mt-1 block w-full"
+        />
+    </div>
+
+    <div class="col-span-6 sm:col-span-4">
+        <InputLabel for="model" value="Modelo" />
+        <TextInput id="model" v-model="form.model"
+            class="mt-1 block w-full"
+        />
+    </div>
+
+    <div class="col-span-6 sm:col-span-4">
+        <InputLabel for="temperature" value="Temperatura" />
+        <TextInput id="temperature" type="number" max="1" min="0" step="0.1" v-model="form.temperature"
+            class="mt-1 block w-full"
+        />
+    </div>
+</template>
