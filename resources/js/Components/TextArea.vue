911f80275142b22e80f27373d4502b16d747dce7
zacktagnan <zacktagnan@gmail.com>
2025-01-13 01:33:15 +0100

    TextArea-Componente.reutilizable.con.Autosize

---
+++ resources/js/Components/TextArea.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import autosize from 'autosize'
+
+defineProps({
+    modelValue: String,
+});
+
+defineEmits(['update:modelValue']);
+
+const textarea = ref(null);
+
+onMounted(() => {
+    if (textarea.value.hasAttribute('autofocus')) {
+        textarea.value.focus();
+    }
+    autosize(textarea.value)
+});
+
+defineExpose({ focus: () => textarea.value.focus() });
+</script>
+
+<template>
+    <textarea ref="textarea"
+        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+        :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
+</template>
--- resources/js/Components/app/Chatbots/Form.vue
+++ resources/js/Components/app/Chatbots/Form.vue
@@ -2,6 +2,7 @@
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue'
 import TextInput from '@/Components/TextInput.vue'
+import TextArea from '@/Components/TextArea.vue'
 
 defineProps({
     form: {
@@ -14,33 +15,26 @@ defineProps({
 <template>
     <div class="col-span-6 sm:col-span-4">
         <InputLabel for="name" value="Nombre" />
-        <TextInput id="name" v-model="form.name"
-            class="mt-1 block w-full"
-        />
+        <TextInput id="name" v-model="form.name" class="mt-1 block w-full" />
         <InputError :message="form.errors.name" class="mt-1" />
     </div>
 
     <div class="col-span-6 sm:col-span-4">
         <InputLabel for="system_prompt" value="System Prompt" />
-        <TextInput id="system_prompt" v-model="form.system_prompt"
-            class="mt-1 block w-full"
-        />
+        <TextArea id="system_prompt" v-model="form.system_prompt" class="mt-1 block w-full" />
         <InputError :message="form.errors.system_prompt" class="mt-1" />
     </div>
 
     <div class="col-span-6 sm:col-span-4">
         <InputLabel for="model" value="Modelo" />
-        <TextInput id="model" v-model="form.model"
-            class="mt-1 block w-full"
-        />
+        <TextInput id="model" v-model="form.model" class="mt-1 block w-full" />
         <InputError :message="form.errors.model" class="mt-1" />
     </div>
 
     <div class="col-span-6 sm:col-span-4">
         <InputLabel for="temperature" value="Temperatura" />
         <TextInput id="temperature" type="number" max="1" min="0" step="0.1" v-model="form.temperature"
-            class="mt-1 block w-full"
-        />
+            class="mt-1 block w-full" />
         <InputError :message="form.errors.temperature" class="mt-1" />
     </div>
 </template>
